e001e05e252e347903ace41a4d64d7bdaa8f8ef0
York Yao <me@yorkyao.com>
2022-04-28 08:01:48 +0800

    refactor: add model register

--- dev/combination-2.story.tsx
+++ dev/combination-2.story.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { Circle, CircleEditBar, PolylineEditBar, Position, useCircleClickCreate, useCircleEdit, useDragMove, useDragRotate, useDragSelect, useKey, useLineClickCreate, usePolylineEdit, useUndoRedo } from '../src'
-import { Content, getContentByClickPosition, getContentsByClickTwoPositions, moveContent, rotateContent } from './util-2'
+import { getContentByClickPosition, getContentsByClickTwoPositions, moveContent, rotateContent } from './util-2'
 import { PixiRenderer } from './pixi-renderer'
 import { SvgRenderer } from './svg-renderer'
 import produce from 'immer'
+import { Content } from './model-2'
 
 const operations = ['2 points', '3 points', 'center radius', 'center diameter', 'line', 'polyline', 'move', 'delete', 'rotate', 'clone'] as const
 type Operation = typeof operations[number]
@@ -14,17 +15,16 @@ const initialState = draftState ? JSON.parse(draftState) as Content[] : []
 export default () => {
   const [operation, setOperation] = React.useState<Operation>()
   const [nextOperation, setNextOperation] = React.useState<Operation>()
-  const [drawingContent, setDrawingContent] = React.useState<Content>()
   const { state, setState, undo, redo, canRedo, canUndo, stateIndex } = useUndoRedo(initialState)
   const [selectedContents, setSelectedContents] = React.useState<number[]>([])
   const [hoveringContent, setHoveringContent] = React.useState<number>(-1)
   const [renderTarget, setRenderTarget] = React.useState<'pixi' | 'svg'>('pixi')
 
-  const { setCircleEditOffset, setPolylineEditOffset, setMoveOffset, setCloneOffset, setRotateOffset, previewContents, rotateOffset } = usePreview(state, selectedContents)
+  const { setCircleEditOffset, setPolylineEditOffset, setLineEditOffset, setMoveOffset, setCloneOffset, setRotateOffset, previewContents, rotateOffset, setPolylineCreate, setCircleCreate, setLineCreate } = usePreview(state, selectedContents)
 
   const { onCircleClickCreateClick, onCircleClickCreateMove, circleClickCreateInput } = useCircleClickCreate(
     operation === '2 points' || operation === '3 points' || operation === 'center diameter' || operation === 'center radius' ? operation : undefined,
-    (c) => setDrawingContent(c ? { ...c, type: 'circle' } : undefined),
+    setCircleCreate,
     (c) => {
       setState((draft) => {
         draft.push({ ...c, type: 'circle' })
@@ -32,18 +32,23 @@ export default () => {
       setOperation(undefined)
     },
   )
-
-  const { onLineClickCreateClick, onLineClickCreateMove, lineClickCreateInput } = useLineClickCreate(
-    operation === 'polyline' || operation === 'line',
-    (c) => setDrawingContent(c ? { points: c, type: 'polyline' } : undefined),
+  const { onLineClickCreateClick: onPolylineClickCreateClick, onLineClickCreateMove: onPolylineClickCreateMove, lineClickCreateInput: polylineClickCreateInput } = useLineClickCreate(
+    operation === 'polyline',
+    (c) => setPolylineCreate(c ? { points: c } : undefined),
     (c) => {
       setState((draft) => {
-        if (operation === 'polyline') {
-          draft.push({ points: c, type: 'polyline' })
-        } else if (operation === 'line') {
-          for (let i = 1; i < c.length; i++) {
-            draft.push({ points: [c[i - 1], c[i]], type: 'line' })
-          }
+        draft.push({ points: c, type: 'polyline' })
+      })
+      setOperation(undefined)
+    },
+  )
+  const { onLineClickCreateClick, onLineClickCreateMove, lineClickCreateInput } = useLineClickCreate(
+    operation === 'line',
+    (c) => setLineCreate(c ? { points: c } : undefined),
+    (c) => {
+      setState((draft) => {
+        for (let i = 1; i < c.length; i++) {
+          draft.push({ points: [c[i - 1], c[i]], type: 'line' })
         }
       })
       setOperation(undefined)
@@ -58,6 +63,8 @@ export default () => {
 
   const { onStartEditCircle, circleEditMask } = useCircleEdit<number>(setCircleEditOffset, () => setState(() => previewContents))
   const { onStartEditPolyline, polylineEditMask } = usePolylineEdit<number>(setPolylineEditOffset, () => setState(() => previewContents))
+  const { onStartEditPolyline: onStartEditLine, polylineEditMask: lineEditMask } = usePolylineEdit<number>(setLineEditOffset, () => setState(() => previewContents))
+
   const { onStartMove, dragMoveMask } = useDragMove(setMoveOffset, () => setState(() => previewContents))
   const { onStartMove: onStartClone, dragMoveMask: dragCloneMask } = useDragMove(setCloneOffset, () => setState(() => previewContents), { clone: true })
   const { onStartRotate, dragRotateMask } = useDragRotate((f) => setRotateOffset({ ...rotateOffset, angle: f ? f - 90 : undefined }), () => setState(() => previewContents))
@@ -90,6 +97,7 @@ export default () => {
   const onClick = (e: React.MouseEvent<HTMLOrSVGElement, MouseEvent>) => {
     onCircleClickCreateClick(e)
     onLineClickCreateClick(e)
+    onPolylineClickCreateClick(e)
     if (operation === 'move') {
       onStartMove(e)
       setOperation(undefined)
@@ -112,12 +120,13 @@ export default () => {
   const onMouseMove = (e: React.MouseEvent<HTMLOrSVGElement, MouseEvent>) => {
     onCircleClickCreateMove(e)
     onLineClickCreateMove(e)
+    onPolylineClickCreateMove(e)
     if (!operation) {
       setHoveringContent(getContentByClickPosition(state, { x: e.clientX, y: e.clientY }, selectedContents))
     }
   }
   const onStartOperation = (p: Operation) => {
-    if ((p === 'move' || p === 'rotate' || p === 'delete') && selectedContents.length === 0) {
+    if ((p === 'move' || p === 'rotate' || p === 'delete' || p === 'clone') && selectedContents.length === 0) {
       setOperation(undefined)
       setNextOperation(p)
       return
@@ -133,34 +142,42 @@ export default () => {
   const Render = renderTarget === 'pixi' ? PixiRenderer : SvgRenderer
 
   return (
-    <div style={{ height: '100%', cursor: 'crosshair' }} onMouseMove={onMouseMove}>
-      <Render
-        contents={previewContents}
-        drawingContent={drawingContent}
-        selectedContents={selectedContents}
-        hoveringContent={hoveringContent}
-        onClick={onClick}
-      />
-      {previewContents.map((s, i) => {
-        if (selectedContents.includes(i)) {
-          if (s.type === 'circle') {
-            return <CircleEditBar key={i} x={s.x} y={s.y} radius={s.r} onClick={(e, type, cursor) => onStartEditCircle(e, { ...s, type, cursor, data: i })} />
+    <div style={{ height: '100%' }}>
+      <div style={{ cursor: 'crosshair' }} onMouseMove={onMouseMove}>
+        <Render
+          contents={previewContents}
+          selectedContents={selectedContents}
+          hoveringContent={hoveringContent}
+          onClick={onClick}
+        />
+        {previewContents.map((s, i) => {
+          if (selectedContents.includes(i)) {
+            if (s.type === 'circle') {
+              return <CircleEditBar key={i} x={s.x} y={s.y} radius={s.r} onClick={(e, type, cursor) => onStartEditCircle(e, { ...s, type, cursor, data: i })} />
+            }
+            if (s.type === 'polyline') {
+              return <PolylineEditBar key={i} points={s.points} onClick={(e, pointIndexes) => onStartEditPolyline(e, pointIndexes, i)} />
+            }
+            if (s.type === 'line') {
+              return <PolylineEditBar key={i} points={s.points} onClick={(e, pointIndexes) => onStartEditLine(e, pointIndexes, i)} />
+            }
           }
-          if (s.type === 'line' || s.type === 'polyline') {
-            return <PolylineEditBar key={i} points={s.points} onClick={(e, pointIndexes) => onStartEditPolyline(e, pointIndexes, i)} />
-          }
-        }
-        return null
-      })}
+          return null
+        })}
+      </div>
       {operations.map((p) => <button onClick={() => onStartOperation(p)} key={p} style={{ position: 'relative', borderColor: p === operation || p === nextOperation ? 'red' : undefined }}>{p}</button>)}
       <button disabled={!canUndo} onClick={() => undo()} style={{ position: 'relative' }}>undo</button>
       <button disabled={!canRedo} onClick={() => redo()} style={{ position: 'relative' }}>redo</button>
       <button onClick={() => setRenderTarget(renderTarget === 'pixi' ? 'svg' : 'pixi')} style={{ position: 'relative' }}>{renderTarget}</button>
       {circleClickCreateInput}
       {lineClickCreateInput}
-      {dragSelectMask}
+      {polylineClickCreateInput}
+
       {circleEditMask}
       {polylineEditMask}
+      {lineEditMask}
+
+      {dragSelectMask}
       {dragMoveMask}
       {dragRotateMask}
       {dragCloneMask}
@@ -169,10 +186,16 @@ export default () => {
 }
 
 function usePreview(state: Content[], selectedContents: number[]) {
+  const [circleCreate, setCircleCreate] = React.useState<Circle>()
+  const [polylineCreate, setPolylineCreate] = React.useState<{ points: Position[] }>()
+  const [lineCreate, setLineCreate] = React.useState<{ points: Position[] }>()
+
   const [circleEditOffset, setCircleEditOffset] = React.useState<Circle & { data?: number }>({ x: 0, y: 0, r: 0 })
-  const [polyineEditOffset, setPolylineEditOffset] = React.useState<Position & { pointIndexes: number[], data?: number }>()
+  const [polylineEditOffset, setPolylineEditOffset] = React.useState<Position & { pointIndexes: number[], data?: number }>()
+  const [lineEditOffset, setLineEditOffset] = React.useState<Position & { pointIndexes: number[], data?: number }>()
+
   const [moveOffset, setMoveOffset] = React.useState<Position>({ x: 0, y: 0 })
-  const [cloneOffset, setCloneOffset] = React.useState<Position>()
+  const [cloneOffset, setCloneOffset] = React.useState<Position>({ x: 0, y: 0 })
   const [rotateOffset, setRotateOffset] = React.useState<Position & { angle?: number }>({ x: 0, y: 0 })
 
   const previewContents = produce(state, (draft) => {
@@ -184,12 +207,21 @@ function usePreview(state: Content[], selectedContents: number[]) {
         content.r += circleEditOffset.r
       }
     }
-    if (polyineEditOffset?.data !== undefined) {
-      const content = draft[polyineEditOffset.data]
-      if (content.type === 'line' || content.type === 'polyline') {
-        for (const pointIndex of polyineEditOffset.pointIndexes) {
-          content.points[pointIndex].x += polyineEditOffset.x
-          content.points[pointIndex].y += polyineEditOffset.y
+    if (polylineEditOffset?.data !== undefined) {
+      const content = draft[polylineEditOffset.data]
+      if (content.type === 'polyline') {
+        for (const pointIndex of polylineEditOffset.pointIndexes) {
+          content.points[pointIndex].x += polylineEditOffset.x
+          content.points[pointIndex].y += polylineEditOffset.y
+        }
+      }
+    }
+    if (lineEditOffset?.data !== undefined) {
+      const content = draft[lineEditOffset.data]
+      if (content.type === 'line') {
+        for (const pointIndex of lineEditOffset.pointIndexes) {
+          content.points[pointIndex].x += lineEditOffset.x
+          content.points[pointIndex].y += lineEditOffset.y
         }
       }
     }
@@ -202,7 +234,7 @@ function usePreview(state: Content[], selectedContents: number[]) {
         if (rotateOffset.angle) {
           rotateContent(content, rotateOffset, -rotateOffset.angle)
         }
-        if (cloneOffset) {
+        if (cloneOffset.x !== 0 || cloneOffset.y !== 0) {
           clonedContents.push(produce(content, (d) => {
             moveContent(d, cloneOffset)
           }))
@@ -210,15 +242,33 @@ function usePreview(state: Content[], selectedContents: number[]) {
       }
     })
     draft.push(...clonedContents)
+
+    if (circleCreate) {
+      draft.push({ type: 'circle', ...circleCreate })
+    }
+    if (polylineCreate) {
+      draft.push({ type: 'polyline', ...polylineCreate })
+    }
+    if (lineCreate) {
+      for (let i = 1; i < lineCreate.points.length; i++) {
+        draft.push({ points: [lineCreate.points[i - 1], lineCreate.points[i]], type: 'line' })
+      }
+    }
   })
 
   return {
-    rotateOffset,
     setCircleEditOffset,
     setPolylineEditOffset,
+    setLineEditOffset,
+
+    rotateOffset,
     setMoveOffset,
     setCloneOffset,
     setRotateOffset,
     previewContents,
+
+    setCircleCreate,
+    setPolylineCreate,
+    setLineCreate,
   }
 }
--- dev/util-2.ts
+++ dev/util-2.ts
@@ -1,10 +1,5 @@
-import { Circle, getPointAndLineMinimumDistance, getPointAndRegionMaximumDistance, getPointAndRegionMinimumDistance, getTwoNumbersDistance, getTwoPointsDistance, getTwoPointsFormRegion, lineIntersectWithTwoPointsFormRegion, pointIsInRegion, Position } from '../src'
-import { rotatePositionByCenter } from './util'
-
-export type Content =
-  | { type: 'circle' } & Circle
-  | { type: 'polyline', points: Position[] }
-  | { type: 'line', points: Position[] }
+import { getTwoPointsFormRegion, Position } from '../src'
+import { Content, getModel } from './model-2'
 
 export function getContentByClickPosition(
   contents: Content[],
@@ -17,17 +12,8 @@ export function getContentByClickPosition(
       continue
     }
     const content = contents[i]
-    if (content.type === 'circle') {
-      if (getTwoNumbersDistance(getTwoPointsDistance(content, position), content.r) <= delta) {
-        return i
-      }
-    } else if (content.type === 'polyline' || content.type === 'line') {
-      for (let j = 1; j < content.points.length; j++) {
-        const minDistance = getPointAndLineMinimumDistance(position, content.points[j - 1], content.points[j])
-        if (minDistance <= delta) {
-          return i
-        }
-      }
+    if (getModel(content.type).canSelectByPosition(content, position, delta)) {
+      return i
     }
   }
   return -1
@@ -42,53 +28,17 @@ export function getContentsByClickTwoPositions(
   const region = getTwoPointsFormRegion(startPosition, endPosition)
   const partial = startPosition.x > endPosition.x
   contents.forEach((content, i) => {
-    if (content.type === 'circle') {
-      if ([
-        { x: content.x - content.r, y: content.y - content.r },
-        { x: content.x + content.r, y: content.y + content.r },
-      ].every((p) => pointIsInRegion(p, region))) {
-        result.push(i)
-      } else if (partial) {
-        const minDistance = getPointAndRegionMinimumDistance(content, region)
-        const maxDistance = getPointAndRegionMaximumDistance(content, region)
-        if (minDistance <= content.r && maxDistance >= content.r) {
-          result.push(i)
-        }
-      }
-    } else if (content.type === 'polyline' || content.type === 'line') {
-      if (content.points.every((p) => pointIsInRegion(p, region))) {
-        result.push(i)
-      } else if (partial) {
-        for (let j = 1; j < content.points.length; j++) {
-          if (lineIntersectWithTwoPointsFormRegion(content.points[j - 1], content.points[j], region)) {
-            result.push(i)
-            break
-          }
-        }
-      }
+    if (getModel(content.type).canSelectByTwoPositions(content, region, partial)) {
+      result.push(i)
     }
   })
   return result
 }
 
 export function moveContent(content: Content, offset: Position) {
-  if (content.type === 'circle') {
-    content.x += offset.x
-    content.y += offset.y
-  } else if (content.type === 'line' || content.type === 'polyline') {
-    for (const point of content.points) {
-      point.x += offset.x
-      point.y += offset.y
-    }
-  }
+  getModel(content.type).move(content, offset)
 }
 
 export function rotateContent(content: Content, center: Position, angle: number) {
-  if (content.type === 'circle') {
-    const p = rotatePositionByCenter(content, center, angle)
-    content.x = p.x
-    content.y = p.y
-  } else if (content.type === 'line' || content.type === 'polyline') {
-    content.points = content.points.map((p) => rotatePositionByCenter(p, center, angle))
-  }
+  getModel(content.type).rotate(content, center, angle)
 }
