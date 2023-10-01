c5aaf873a61086f1ae32ec562387e5fa9bba9ad0
york <me@yorkyao.com>
2023-10-02 07:02:29 +0800

    feat: cubic equation, tangency point to ellipse

--- spec/equation-calculater.ts
+++ spec/equation-calculater.ts
@@ -1,9 +1,9 @@
 import test from 'ava'
-import { calculateEquation2, calculateEquation4, isZero } from '../src'
+import { calculateEquation2, calculateEquation3, calculateEquation4, isZero } from '../src'
 
 test('calculate2', (t) => {
   function calculate(b: number, c: number) {
-    const sx = calculateEquation2(b, c)
+    const sx = calculateEquation2(1, b, c)
     for (const x of sx) {
       t.true(isZero(x ** 2 + b * x + c))
     }
@@ -11,10 +11,20 @@ test('calculate2', (t) => {
   calculate(5, 6)
 })
 
+test('calculate3', (t) => {
+  function calculate(b: number, c: number, d: number) {
+    const sx = calculateEquation3(1, b, c, d)
+    for (const x of sx) {
+      t.true(isZero(x ** 3 + b * x ** 2 + c * x + d))
+    }
+  }
+  calculate(5, 6, -40)
+})
+
 test('calculate4', (t) => {
   function calculate(b: number, c: number, d: number, x0: number) {
     const e = -(x0 ** 4 + b * x0 ** 3 + c * x0 ** 2 + d * x0)
-    const sx = calculateEquation4(b, c, d, e)
+    const sx = calculateEquation4(1, b, c, d, e)
     t.true(sx.some(x => isZero(x - x0)))
     for (const x of sx) {
       t.true(isZero(x ** 4 + b * x ** 3 + c * x ** 2 + d * x + e, 0.000001))
--- spec/intersection.ts
+++ spec/intersection.ts
@@ -1,5 +1,5 @@
 import test from 'ava'
-import { getCircleEllipseIntersectionPoints, getTwoEllipseIntersectionPoints } from '../src'
+import { getCircleEllipseIntersectionPoints, getTangencyPointToEllipse, getTwoEllipseIntersectionPoints } from '../src'
 
 test('getCircleEllipseIntersectionPoints', (t) => {
   t.snapshot(getCircleEllipseIntersectionPoints({ x: 1412, y: 122, r: 70 }, { cx: 1404, cy: 119, rx: 104, ry: 49, angle: 30 }))
@@ -7,4 +7,9 @@ test('getCircleEllipseIntersectionPoints', (t) => {
 
 test('getTwoEllipseIntersectionPoints', (t) => {
   t.snapshot(getTwoEllipseIntersectionPoints({ cx: 1417, cy: 128, rx: 115, ry: 40, angle: 128 }, { cx: 1404, cy: 119, rx: 104, ry: 49, angle: 30 }))
+})
+
+test('getTangencyPointToEllipse', (t) => {
+  t.snapshot(getTangencyPointToEllipse({ x: 1294, y: 126 }, { cx: 1404, cy: 119, rx: 104, ry: 49, angle: 30 }))
+  t.snapshot(getTangencyPointToEllipse({ x: 50, y: 100 }, { cx: 0, cy: 0, rx: 100, ry: 50, angle: 90 }))
 })
