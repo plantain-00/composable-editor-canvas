4d2d328cdecf877f83054e458e0ed50b2f8eeb32
york <me@yorkyao.com>
2024-05-18 08:02:58 +0800

    feat: image editor

---
+++ dev/image-editor.story.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { ImageEditor, useWindowSize } from "../src"
+
+export default () => {
+  const size = useWindowSize()
+  return (
+    <ImageEditor
+      src='https://farm9.staticflickr.com/8873/18598400202_3af67ef38f_z_d.jpg'
+      width={size.width / 2}
+    />
+  )
+}
