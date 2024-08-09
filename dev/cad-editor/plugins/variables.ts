827cd2cf2a8e1a29933d4aa952300be9d6f73ff5
plantain-00 <plantain-00@users.noreply.github.com>
2024-08-09 00:11:58 +0000

    feat: parabola plugin 48e95e3e3e4992c1f9fb30a7262fe56fe4d2f2fe

--- dev/cad-editor/plugins/variables.ts
+++ dev/cad-editor/plugins/variables.ts
@@ -7483,6 +7483,126 @@ export {
   getCommand
 };
 `,
+`// dev/cad-editor/plugins/parabola.plugin.tsx
+function getModel(ctx) {
+  const ParabolaContent = ctx.and(ctx.BaseContent("parabola"), ctx.StrokeFields, ctx.ParabolaSegment, ctx.SegmentCountFields);
+  const getRefIds = (content) => ctx.getStrokeRefIds(content);
+  return {
+    type: "parabola",
+    ...ctx.strokeModel,
+    move(content, offset) {
+      ctx.movePoint(content, offset);
+    },
+    render(content, renderCtx) {
+      var _a;
+      const { options, target } = ctx.getStrokeRenderOptionsFromRenderContext(content, renderCtx);
+      const segmentCount = (_a = content.segmentCount) != null ? _a : ctx.defaultSegmentCount;
+      const rate = (content.t2 - content.t1) / segmentCount;
+      const points = [];
+      const matrix = ctx.getCoordinateMatrix2D(content, ctx.angleToRadian(content.angle - 90));
+      for (let i = 0; i <= segmentCount; i++) {
+        const x = content.t1 + i * rate;
+        const y = 2 * content.p * x ** 2;
+        const vec = ctx.getCoordinateVec2D({ x, y });
+        const p = ctx.matrix.multiplyVec(matrix, vec);
+        points.push({ x: p[0], y: p[1] });
+      }
+      return target.renderPolyline(points, options);
+    },
+    isValid: (c, p) => ctx.validate(c, ParabolaContent, p),
+    getRefIds,
+    updateRefId: ctx.updateStrokeRefIds,
+    deleteRefId: ctx.deleteStrokeRefIds
+  };
+}
+function isParabolaContent(content) {
+  return content.type === "parabola";
+}
+function getCommand(ctx) {
+  const React = ctx.React;
+  const icon = /* @__PURE__ */ React.createElement("svg", { xmlns: "http://www.w3.org/2000/svg", viewBox: "0 0 100 100" }, /* @__PURE__ */ React.createElement("polygon", { points: "52,5 97,50 52,96 6,50", strokeWidth: "5", strokeMiterlimit: "10", strokeLinejoin: "miter", strokeLinecap: "butt", fill: "none", stroke: "currentColor" }));
+  return {
+    name: "create parabola",
+    icon,
+    useCommand({ onEnd, type, strokeStyleId }) {
+      const [content, setContent] = React.useState();
+      const [status, setStatus] = React.useState("position");
+      const reset = () => {
+        setContent(void 0);
+        setStatus("position");
+      };
+      const assistentContents = [];
+      if (content) {
+        assistentContents.push(content);
+      }
+      return {
+        onStart() {
+          if (type !== "create parabola") return;
+          if (status === "position") {
+            setStatus("angle");
+          } else if (status === "angle") {
+            setStatus("t1");
+          } else if (status === "t1") {
+            setStatus("t2");
+          } else if (status === "t2") {
+            onEnd({
+              updateContents: (contents) => contents.push(content)
+            });
+            reset();
+          }
+        },
+        onMove(p) {
+          if (type !== "create parabola") return;
+          if (!content) {
+            setContent({
+              type: "parabola",
+              x: p.x,
+              y: p.y,
+              p: 0.01,
+              t1: -100,
+              t2: 100,
+              angle: -90,
+              strokeStyleId
+            });
+          } else if (status === "position") {
+            setContent({
+              ...content,
+              x: p.x,
+              y: p.y
+            });
+          } else if (status === "angle") {
+            setContent({
+              ...content,
+              angle: ctx.radianToAngle(ctx.getTwoPointsRadian(p, content))
+            });
+          } else if (status === "t1") {
+            const x = ctx.getPerpendicularParamToLine2D(p, content, ctx.angleToRadian(content.angle - 90));
+            const y = ctx.getPerpendicularParamToLine2D(p, content, ctx.angleToRadian(content.angle));
+            setContent({
+              ...content,
+              t1: x,
+              p: Math.abs(y) / x / x / 2
+            });
+          } else if (status === "t2") {
+            setContent({
+              ...content,
+              t2: ctx.getPerpendicularParamToLine2D(p, content, ctx.angleToRadian(content.angle - 90))
+            });
+          }
+        },
+        assistentContents,
+        reset
+      };
+    },
+    selectCount: 0
+  };
+}
+export {
+  getCommand,
+  getModel,
+  isParabolaContent
+};
+`,
 `// dev/cad-editor/plugins/coordinate-axis.plugin.tsx
 function isCoordinateAxisContent(content) {
   return content.type === "coordinate axis";
