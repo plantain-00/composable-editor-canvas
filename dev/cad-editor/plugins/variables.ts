b953f6e1675486f45d3369a0d714c7166f90a1d2
york <me@yorkyao.com>
2024-11-14 07:11:32 +0800

    feat: get circle tangent to 2 geometry lines near param

--- dev/cad-editor/plugins/variables.ts
+++ dev/cad-editor/plugins/variables.ts
@@ -2733,6 +2733,7 @@ function getCommand(ctx) {
   const React = ctx.React;
   const icon = /* @__PURE__ */ React.createElement("svg", { xmlns: "http://www.w3.org/2000/svg", viewBox: "0 0 100 100" }, /* @__PURE__ */ React.createElement("polyline", { points: "10,87 89,87", strokeWidth: "5", strokeMiterlimit: "10", strokeLinejoin: "miter", strokeLinecap: "butt", fill: "none", stroke: "currentColor" }), /* @__PURE__ */ React.createElement("circle", { cx: "17", cy: "40", r: "16", strokeWidth: "5", strokeMiterlimit: "10", strokeLinejoin: "miter", strokeLinecap: "butt", fill: "none", stroke: "currentColor" }), /* @__PURE__ */ React.createElement("circle", { cx: "60", cy: "57", r: "30", strokeWidth: "5", strokeMiterlimit: "10", strokeLinejoin: "miter", strokeLinecap: "butt", fill: "none", stroke: "currentColor" }));
   const contentSelectable = (c) => isCircleContent(c) || isArcContent(c) || isLineContent(c);
+  const icon2 = /* @__PURE__ */ React.createElement("svg", { xmlns: "http://www.w3.org/2000/svg", viewBox: "0 0 100 100" }, /* @__PURE__ */ React.createElement("polyline", { points: "10,87 89,87", strokeWidth: "5", strokeMiterlimit: "10", strokeLinejoin: "miter", strokeLinecap: "butt", fill: "none", stroke: "currentColor" }), /* @__PURE__ */ React.createElement("circle", { cx: "17", cy: "40", r: "16", strokeWidth: "5", strokeMiterlimit: "10", strokeLinejoin: "miter", strokeLinecap: "butt", fill: "none", stroke: "currentColor" }), /* @__PURE__ */ React.createElement("circle", { cx: "60", cy: "57", r: "30", strokeWidth: "5", strokeMiterlimit: "10", strokeLinejoin: "miter", strokeLinecap: "butt", fill: "none", stroke: "currentColor" }), /* @__PURE__ */ React.createElement("circle", { cx: "33", cy: "46", r: "8", fillOpacity: "1", strokeOpacity: "1", fill: "currentColor", stroke: "none" }), /* @__PURE__ */ React.createElement("circle", { cx: "60", cy: "87", r: "8", fillOpacity: "1", strokeOpacity: "1", fill: "currentColor", stroke: "none" }));
   const command = {
     name: "create tangent tangent radius circle",
     useCommand({ onEnd, type, selected, scale }) {
@@ -2907,6 +2908,106 @@ function getCommand(ctx) {
         };
       },
       selectCount: 0
+    },
+    {
+      name: "create tangent tangent radius circle at points",
+      useCommand({ onEnd, type, scale, contents }) {
+        const [start, setStart] = React.useState();
+        const [cursor, setCursor] = React.useState();
+        const [radius, setRadius] = React.useState();
+        const [result, setResult] = React.useState();
+        const assistentContents = [];
+        if (start && cursor && type) {
+          assistentContents.push({
+            points: [start.point, cursor],
+            type: "line",
+            dashArray: [4 / scale]
+          });
+        }
+        if (result) {
+          assistentContents.push({ ...result, type: "circle" });
+        }
+        const getTarget = (point, id, param) => {
+          var _a, _b, _c;
+          const content = contents[id];
+          if (!content) return;
+          const lines = (_c = (_b = (_a = ctx.getContentModel(content)) == null ? void 0 : _a.getGeometries) == null ? void 0 : _b.call(_a, content, contents)) == null ? void 0 : _c.lines;
+          if (!lines) return;
+          const index = Math.floor(param);
+          return { point, line: lines[index], param: param - index };
+        };
+        let message = "";
+        if (type && !radius) {
+          message = "input radius";
+        }
+        const { input, setInputPosition, setCursorPosition, clearText, resetInput } = ctx.useCursorInput(message, type ? (e, text) => {
+          if (e.key === "Enter") {
+            const r = +text;
+            if (!isNaN(r)) {
+              setRadius(r);
+              clearText();
+              resetInput();
+            }
+          }
+        } : void 0);
+        const reset = () => {
+          setStart(void 0);
+          setResult(void 0);
+          setCursor(void 0);
+          setRadius(void 0);
+          clearText();
+          resetInput();
+        };
+        return {
+          onStart(p, target) {
+            if (!type) return;
+            if (!target) return;
+            if (target.param === void 0) return;
+            if (!start) {
+              setStart(getTarget(p, target.id, target.param));
+            } else if (result) {
+              onEnd({
+                updateContents: (contents2) => {
+                  contents2.push({ ...result, type: "circle" });
+                }
+              });
+              reset();
+            }
+          },
+          onMove(p, viewportPosition, target) {
+            if (!type) return;
+            setCursor(p);
+            setResult(void 0);
+            setCursorPosition(p);
+            setInputPosition(viewportPosition || p);
+            if (!radius) return;
+            if (!target) return;
+            if (target.param === void 0) return;
+            if (!start) return;
+            const end = getTarget(p, target.id, target.param);
+            if (!end) return;
+            const center = ctx.getTwoGeneralFormLinesIntersectionPoint(
+              ctx.pointAndDirectionToGeneralFormLine(
+                start.point,
+                ctx.getGeometryLineTangentRadianAtParam(start.param, start.line)
+              ),
+              ctx.pointAndDirectionToGeneralFormLine(
+                end.point,
+                ctx.getGeometryLineTangentRadianAtParam(end.param, end.line)
+              )
+            );
+            if (!center) return;
+            const circle = ctx.getCircleTangentTo2GeometryLinesNearParam(start.line, end.line, radius, start.param, end.param, center);
+            if (!circle) return;
+            setResult({ ...circle, r: radius });
+          },
+          input,
+          assistentContents,
+          reset
+        };
+      },
+      selectCount: 0,
+      icon: icon2
     }
   ];
 }
